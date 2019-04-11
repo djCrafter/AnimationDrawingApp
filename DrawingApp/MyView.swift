276f48f45dc8a3bac306e215daa1bdb085ba3344
Crafter <godhedin2000@gmail.com>
2019-04-11 12:29:23 +0300

    Second Commit

--- DrawingApp/MyView.swift
+++ DrawingApp/MyView.swift
@@ -10,12 +10,24 @@ import UIKit
 
 
 class MyView : UIView {
-    var sides: Int?
-    init(sides: Int, point: CGPoint, backgroundColor: UIColor) {
-        super.init(frame: CGRect(x: point.x, y: point.y, width: 100, height: 150))
-        self.sides = sides
-        self.backgroundColor = backgroundColor
+    
+    @IBInspectable
+    var sides: Int = 0
+    
+    var isViewBack = false {
+        didSet{
+            setNeedsDisplay(); setNeedsLayout()
+        }
     }
+    
+    
+//    init(sides: Int, point: CGPoint, backgroundColor: UIColor) {
+//        super.init(frame: CGRect(x: point.x, y: point.y, width: 100, height: 150))
+//        self.sides = sides
+//        self.backgroundColor = backgroundColor              
+//    }
+    
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -24,10 +36,16 @@ class MyView : UIView {
     override func draw(_ rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()!
         
-        if sides! > 2 {
-            drawFigure(context: context, rect: rect, sides: sides!)
+        if(isViewBack) {
+        if sides > 2 {
+            drawFigure(context: context, rect: rect, sides: sides)
         }
-        drawText(rect: rect, text: String(sides!))
+        drawText(rect: rect, text: String(sides))
+        } else {
+            if let backImage = UIImage(named: "cardback"){
+                backImage.draw(in: bounds)
+            }
+        }
     }
     
     func drawFigure(context: CGContext, rect: CGRect, sides: Int){
@@ -35,25 +53,38 @@ class MyView : UIView {
         let radius = Double(rect.maxX - rect.minX)/2
         let length = sin(Double.pi / Double(sides)) * 2.0 * radius
         let angle = -360.0/Double(sides)
-        var point = (x: Double(rect.maxX/2) + length/2, y: Double(rect.maxY) - 7)
-
+        let constPoint = (x: Double(rect.maxX/2) + length/2, y: Double(rect.maxY) - 15)
+        var point = constPoint
+        
+        var pointArr : [CGPoint] = []
+        pointArr.append(CGPoint(x: point.x, y: point.y))
+        
         context.move(to: CGPoint(x: point.x, y: point.y))
-
+        
         for i in 1...sides{
+            
             let sinFi = sin(Double(i) * angle * Double.pi / 180.0)
             let cosFi = cos(Double(i) * angle * Double.pi / 180.0)
             
             point.x = point.x + length * cosFi
             point.y = point.y + length * sinFi
             
-            context.addLine(to: CGPoint(x: point.x, y: point.y))
+            let tempPoint = CGPoint(x: point.x, y: point.y)
+            pointArr.append(tempPoint)
+            context.addLine(to: tempPoint)
         }
-        context.closePath()
+        
+        context.setFillColor(#colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1))
+        context.fillPath();
+        
+        context.setLineWidth(3)
+        context.setStrokeColor(#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1))
+        context.addLines(between: pointArr)
         context.strokePath()
     }
     
     func drawText(rect: CGRect, text: String){
-        let textRect = CGRect(x: (rect.maxX / 2) - 7, y: 25, width: 25, height: 25)
+        let textRect = CGRect(x: (rect.maxX / 2) - 7, y: (rect.maxY / 2), width: 25, height: 25)
         let attribute: [NSAttributedString.Key : Any] = [.font: UIFont.preferredFont(forTextStyle: .body).withSize(20)]
         (text as NSString).draw(in: textRect, withAttributes: attribute)
     }
--- DrawingApp/ViewController.swift
+++ DrawingApp/ViewController.swift
@@ -8,27 +8,17 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let myView1 = MyView(sides: 0, point: CGPoint(x: 30, y: 100), backgroundColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1))
-        let myView2 = MyView(sides: 1, point: CGPoint(x: 150, y: 100), backgroundColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1))
-        let myView3 = MyView(sides: 2, point: CGPoint(x: 270, y: 100), backgroundColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1))
-        let myView4 = MyView(sides: 3, point: CGPoint(x: 30, y: 300), backgroundColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1))
-        let myView5 = MyView(sides: 5, point: CGPoint(x: 150, y: 300), backgroundColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1))
-        let myView6 = MyView(sides: 8, point: CGPoint(x: 270, y: 300), backgroundColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1))
-     
-        
-        self.view.addSubview(myView1)
-        self.view.addSubview(myView2)
-        self.view.addSubview(myView3)
-        self.view.addSubview(myView4)
-        self.view.addSubview(myView5)
-        self.view.addSubview(myView6)
-        
-                
     }
+    
+    @IBOutlet var myViews: [MyView]!
+    
+    
+    
 }
 
